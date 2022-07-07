d1233460d6a4088037628fdb0b87b1e182ce1706
miracleonyenma <miracleficient@gmail.com>
2022-07-07 16:22:05 +0100

    feat: implement infinite scroll
    With Scroll Event Handlers and Intersection Observer API

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,193 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, onMounted, ref } from "vue";
+
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+
+// GraphQL query
+const ALLSTARSHIPS_QUERY = gql`
+  query AllStarships($first: Int, $after: String) {
+    allStarships(first: $first, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          name
+          starshipClass
+        }
+      }
+    }
+  }
+`;
+
+// create ref for observer
+const observer = ref(null);
+
+// create ref for target element for observer to observe
+const target = ref(null);
+
+// destructure
+const {
+  // result of the query
+  result,
+
+  // loading state of the query
+  loading,
+
+  // query errors, if any
+  error,
+
+  // method to fetch more
+  fetchMore,
+
+  // access to query variables
+  variables,
+} = useQuery(ALLSTARSHIPS_QUERY, { first: 5 });
+
+// computed value to know if there are more pages after the last result
+const hasNextPage = computed(() => result.value.allStarships.pageInfo.hasNextPage);
+
+// console.log({ result, loading, error, variables, hasNextPage });
+
+// function to load more content and update query result
+const loadMore = () => {
+  // fetchMore function from `useQuery` to fetch more content with `updateQuery`
+  fetchMore({
+    // update `after` variable with `endCursor` from previous result
+    variables: {
+      after: result.value?.allStarships.pageInfo.endCursor,
+    },
+
+    // pass previous query result and the new results to `updateQuery`
+    updateQuery: (previousQueryResult, { fetchMoreResult }) => {
+      // define edges and pageInfo from new results
+      const newEdges = fetchMoreResult.allStarships.edges;
+      const pageInfo = fetchMoreResult.allStarships.pageInfo;
+
+      // if newEdges actually have items,
+      return newEdges.length
+        ? // return a reconstruction of the query result with updated values
+          {
+            // spread the value of the previous result
+            ...previousQueryResult,
+            allStarships: {
+              // spread the value of the previous `allStarhips` data into this object
+              ...previousQueryResult.allStarships,
+
+              // concatenate edges
+              edges: [...previousQueryResult.allStarships.edges, ...newEdges],
+
+              // Override with new pageInfo
+              pageInfo,
+            },
+          }
+        : // else, return the previous result
+          previousQueryResult;
+    },
+  });
+};
+
+onMounted(() => {
+  // // listen to the scroll event in the window object (the page)
+  // window.addEventListener(
+  //   "scroll",
+  //   () => {
+  //     // define
+  //     let {
+  //       // the amount useer has scrolled
+  //       scrollTop,
+
+  //       // the height of the page
+  //       scrollHeight,
+
+  //       // the height of viewport
+  //       clientHeight
+  //     } = document.documentElement;
+
+  //     // if user has scrolled to the bottom of the page
+  //     if (scrollTop + clientHeight >= scrollHeight && hasNextPage.value) {
+
+  //       // exccute the loadMore function to fetch more items
+  //       loadMore();
+  //     }
+  //   },
+  //   {
+  //     // indicate that the listener will not cancel the scroll
+  //     passive: true,
+  //   }
+  // );
+
+  // options for observer
+  const options = {
+    threshold: 1.0,
+  };
+
+  // define obseerver
+  observer.value = new IntersectionObserver(([entry]) => {
+
+    // if the target is visible
+    if (entry && entry.isIntersecting) {
+
+      // load more content
+      loadMore();
+    }
+  }, options);
+
+  // define the target to observe
+  observer.value.observe(target.value);
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <ul class="starship-list">
+      <p v-if="error">oops</p>
+
+      <!-- "infinite" list -->
+      <li v-else v-for="starship in result?.allStarships.edges" :key="starship.node.id" class="starship-item">
+        <p>{{ starship.node.name }}</p>
+      </li>
+    </ul>
+
+    <!-- target button, load more manually when clicked -->
+    <button ref="target" @click="loadMore" class="cta">
+      <span v-if="loading">Loading...</span>
+      <span v-else-if="!hasNextPage">That's a wrap!</span>
+      <span v-else>More</span>
+    </button>
   </main>
 </template>
 
-<style>
-@import './assets/base.css';
+<style scoped>
+button {
+  cursor: pointer;
+}
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
+main {
+  width: 100%;
+  max-width: 30rem;
+  margin: auto;
   padding: 2rem;
-
-  font-weight: normal;
+}
+.starship-list {
+  list-style: none;
+  padding: 4rem 0 4rem 0;
 }
 
-header {
-  line-height: 1.5;
+.starship-item {
+  font-size: xx-large;
+  padding: 1rem 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.cta {
+  padding: 0.5rem 1rem;
+  background: var(--vt-c-white-soft);
+  color: var(--color-background-soft);
+  border: none;
+  border-radius: 0.5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
 
-const app = createApp(App)
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+import App from './App.vue'
+import './assets/base.css'
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'https://swapi-gql.netlify.app/.netlify/functions/index'
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
 
 app.use(createPinia())
 
